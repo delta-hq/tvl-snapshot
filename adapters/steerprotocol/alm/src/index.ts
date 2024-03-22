41d5686a06f225d3bbfa373aca47b347d70894f8
Rakesh <bhattrakesh09@gmail.com>
2024-03-22 18:58:58 +0530

    Added steer tvl snapshot

---
+++ adapters/steerprotocol/alm/src/index.ts
@@ -0,0 +1,78 @@
+import { CHAINS, PROTOCOLS } from "./sdk/config";
+import { getDepositorsForAddressByVaultAtBlock, getVaultPositions } from "./sdk/subgraphDetails";
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
+
+import { write } from 'fast-csv';
+import fs from 'fs';
+import stream from 'stream';
+import { promisify } from 'util';
+
+
+
+interface CSVRow {
+  user: string;
+  vaultId: string;
+  block: number;
+  lpvalue: string;
+  poolId: string,
+  positions: number
+}
+
+
+const pipeline = promisify(stream.pipeline);
+
+
+const getData = async () => {
+  const snapshotBlocks = [
+    5339627, 5447950, 5339736
+  ]; //await readBlocksFromCSV('src/sdk/mode_chain_daily_blocks.csv');
+  
+  let csvRows: CSVRow[] = [];
+
+  for (let block of snapshotBlocks) {
+    const depositors = await getDepositorsForAddressByVaultAtBlock(
+      block, "", "", CHAINS.MODE, PROTOCOLS.STEER
+    );
+
+    console.log(`Block: ${block}`);
+    console.log("Positions: ", depositors.length);
+
+
+    const depositorsRow: CSVRow[] = depositors.map((depositor) => {
+      return {
+        user: depositor.account,
+        vaultId: depositor.vault.id,
+        poolId: depositor.vault.pool,
+        block: Number(depositor.blockNumber),
+        lpvalue: depositor.shares.toString()
+      } as CSVRow
+    });
+
+    csvRows = csvRows.concat(depositorsRow);
+  } 
+
+  const vaultIds = csvRows.map(({vaultId}) => vaultId);
+
+  const uniqueVaultIds = [...new Set(vaultIds)];
+
+
+  for (const csvRow of csvRows) {
+    const vaultPositions = await getVaultPositions( CHAINS.MODE, PROTOCOLS.STEER, csvRow.vaultId)
+    csvRow.positions = vaultPositions[0].lowerTick.length;
+  }
+
+  // Write the CSV output to a file
+  const ws = fs.createWriteStream('outputData.csv');
+  write(csvRows, { headers: true }).pipe(ws).on('finish', () => {
+    console.log("CSV file has been written.");
+  });
+};
+
+getData().then(() => {
+  console.log("Done");
+});
+// getPrice(new BigNumber('1579427897588720602142863095414958'), 6, 18); //Uniswap
+// getPrice(new BigNumber('3968729022398277600000000'), 18, 6); //SupSwap
+
